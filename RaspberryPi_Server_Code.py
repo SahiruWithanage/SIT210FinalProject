c5760ff3dbe0090fa2a5d2b376da559a372a9765
SahiruWithanage <165999067+SahiruWithanage@users.noreply.github.com>
2024-06-10 04:35:42 +0530

    Updated_RaspberryPi_Server_Code.py

--- RaspberryPi_Server_Code.py
+++ RaspberryPi_Server_Code.py
@@ -1,24 +1,26 @@
+import uvicorn
 from fastapi import FastAPI, Response
 from pydub import AudioSegment
 import io
+import subprocess
 
 app = FastAPI()
 
-@app.get("/audio")
+@app.get("/stream")
 async def stream_audio():
-    # Load audio file
-    audio_file_path = "/home/s223047862/wavtest.wav"
-    audio = AudioSegment.from_mp3(audio_file_path)
+    def generate():
+        cmd = [
+            'ffmpeg', '-i', '/tmp/librespot_pipe', '-f', 's16le', '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '1', 'pipe:1'
+        ]
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    # Convert audio to WAV format with desired specifications
-    wav_buffer = io.BytesIO()
-    audio.export(wav_buffer, format="wav", bitrate="320k", parameters=["-ar", "44100", "-sample_fmt", "s16"])
+        while True:
+            chunk = process.stdout.read(4096)
+            if not chunk:
+                break
+            yield chunk
 
-    # Return WAV data as response content
-    wav_data = wav_buffer.getvalue()
-
-    return Response(content=wav_data, media_type="audio/wav")
+    return Response(generate(), media_type="audio/wav")
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
